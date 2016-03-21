54c70c2b22847514fdac7ad6ec0865d7ed28a7d5
stefanpeters9 <stefanpeters9@gmail.com>
2016-03-21 19:56:38 -0400

    assembly file

---
+++ BicycleTurnSignal.asm
@@ -0,0 +1,21 @@
+;**** Timer **** 
+TSCR1 EQU $46
+TSCR2 EQU $4D
+TIOS  EQU $40
+TCTL1 EQU $48
+TCTL2 EQU $49
+TFLG1 EQU $4E
+TIE   EQU $4C
+TSCNT EQU $44
+TC4	  EQU $58
+TC1	  EQU $52
+;***************
+
+;*** PORTS **** 
+DDRA  EQU $02
+PORTA EQU $00
+PORTB EQU $01
+DDRB  EQU $03
+PORTM EQU $0250
+DDRM  EQU $0252
+;**************
